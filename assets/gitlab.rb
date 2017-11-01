015fa405c7dfe877f74431453495783944f13093
jubilee2 <jubilee2@gmail.com>
2017-10-31 22:09:29 -0500

    Update gitlab.rb

--- assets/gitlab.rb
+++ assets/gitlab.rb
@@ -14,3 +14,7 @@ eval ENV["GITLAB_OMNIBUS_CONFIG"].to_s
 
 # Load configuration stored in /etc/gitlab/gitlab.rb
 from_file("/etc/gitlab/gitlab.rb")
+
+###! Minimum worker_processes is 2 at this moment
+###! See https://gitlab.com/gitlab-org/gitlab-ce/issues/18771
+unicorn['worker_processes'] = 2
