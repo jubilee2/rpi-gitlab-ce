1bab4755583e25e45c8279c1688cb86e6a996745
Jubilee Tan <jubilee2@gmail.com>
2019-05-06 03:05:31 -0500

    update assets

--- assets/gitlab.rb
+++ assets/gitlab.rb
@@ -2,6 +2,9 @@
 ## Prevent Postgres from trying to allocate 25% of total memory
 postgresql['shared_buffers'] = '1MB'
 
+# Disable Prometheus node_exporter inside Docker.
+node_exporter['enable'] = false
+
 # Manage accounts with docker
 manage_accounts['enable'] = false
 
@@ -9,12 +12,22 @@ manage_accounts['enable'] = false
 host = `hostname`.strip
 external_url "http://#{host}"
 
+# Explicitly disable init detection since we are running on a container
+package['detect_init'] = false
+
 # Load custom config from environment variable: GITLAB_OMNIBUS_CONFIG
-eval ENV["GITLAB_OMNIBUS_CONFIG"].to_s
+# Disabling the cop since rubocop considers using eval to be security risk but
+# we don't have an easy way out, atleast yet.
+eval ENV["GITLAB_OMNIBUS_CONFIG"].to_s # rubocop:disable Security/Eval
 
 # Load configuration stored in /etc/gitlab/gitlab.rb
 from_file("/etc/gitlab/gitlab.rb")
 
 ###! Minimum worker_processes is 2 at this moment
-###! See https://gitlab.com/gitlab-org/gitlab-ce/issues/18771
+###! See https://docs.gitlab.com/omnibus/settings/rpi.html
+# Reduce the number of running workers to the minimum in order to reduce memory usage
 unicorn['worker_processes'] = 2
+sidekiq['concurrency'] = 9
+
+# Turn off monitoring to reduce idle cpu and disk usage
+prometheus_monitoring['enable'] = false
